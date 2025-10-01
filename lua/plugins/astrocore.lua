d9c5dbf3b8b5d5a0c1164bf3cfcbd005f3624e47
nilsrambow <nilsbrodersen@MON-PF4TNVZB-DE.localdomain>
2025-10-01 21:13:21 +0200

    nice md symbols

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -38,6 +38,7 @@ return {
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = true, -- sets vim.opt.wrap
+        conceallevel = 1,
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -25,6 +25,47 @@ return {
         path = "~/vaults/personal",
       },
     },
+          -- Enable UI enhancements
+      ui = {
+        enable = true,  -- Set to false to disable all UI features
+        update_debounce = 200,  -- Update delay in milliseconds
+        max_file_length = 5000,  -- Disable UI for files longer than this
+        
+        -- Checkboxes with different states
+        checkboxes = {
+          [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+          ["x"] = { char = "󰱒", hl_group = "ObsidianDone" },
+          [">"] = { char = "󰧛", hl_group = "ObsidianRightArrow" },
+          ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+          ["!"] = { char = "", hl_group = "ObsidianImportant" },
+        },
+        
+        -- Bullets for lists
+        bullets = { char = "•", hl_group = "ObsidianBullet" },
+        
+        -- External link icon
+        external_link_icon = { char = "󱥫", hl_group = "ObsidianExtLinkIcon" },
+        
+        -- Reference text (for footnotes, etc.)
+        reference_text = { hl_group = "ObsidianRefText" },
+        highlight_text = { hl_group = "ObsidianHighlightText" },
+        tags = { hl_group = "ObsidianTag" },
+        block_ids = { hl_group = "ObsidianBlockID" },
+        
+        hl_groups = {
+          ObsidianTodo = { bold = true, fg = "#f78c6c" },
+          ObsidianDone = { bold = true, fg = "#89ddff" },
+          ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
+          ObsidianTilde = { bold = true, fg = "#ff5370" },
+          ObsidianImportant = { bold = true, fg = "#d73128" },
+          ObsidianBullet = { bold = true, fg = "#89ddff" },
+          ObsidianRefText = { underline = true, fg = "#c792ea" },
+          ObsidianExtLinkIcon = { fg = "#c792ea" },
+          ObsidianTag = { italic = true, fg = "#89ddff" },
+          ObsidianBlockID = { italic = true, fg = "#89ddff" },
+          ObsidianHighlightText = { bg = "#75662e" },
+        },
+      },
       picker = {
     -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
     name = "telescope.nvim",
