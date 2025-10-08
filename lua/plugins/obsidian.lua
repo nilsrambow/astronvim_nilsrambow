f2794ba8526022eeb0589308cf99a67a793dc0f3
nilsrambow <nilsbrodersen@MON-PF4TNVZB-DE.localdomain>
2025-10-08 10:12:07 +0200

    improved obsidian lazy loading (and fixed formatting)

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -2,17 +2,9 @@
 return {
   "obsidian-nvim/obsidian.nvim",
   version = "*",  -- recommended, use latest release instead of latest commit
-  lazy = false,
+  cmd = { "Obsidian" },
   ft = "markdown",
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-  --   -- refer to `:h file-pattern` for more examples
-  --   "BufReadPre path/to/my-vault/*.md",
-  --   "BufNewFile path/to/my-vault/*.md",
-  -- },
-      keys = {
+  keys = {
       { "gf", "<cmd>Obsidian follow<cr>", ft = "markdown", desc = "Follow Obsidian link" },
       { "<leader>Oo", "<cmd>Obsidian open<cr>", ft = "markdown", desc = "Open in Obsidian" },
       { "<leader>On", "<cmd>Obsidian new<cr>", ft = "markdown", desc = "New Obsidian note" },
@@ -22,93 +14,80 @@ return {
       { "<leader>Ot", "<cmd>Obsidian today<cr>", ft = "markdown", desc = "Today's note" },
       { "<leader>Oy", "<cmd>Obsidian yesterday<cr>", ft = "markdown", desc = "Yesterday's note" },
       { "<leader>Ol", "<cmd>Obsidian links<cr>", ft = "markdown", desc = "Show links" },
-    },
-    opts = {
-    legacy_commands = false,
+  },
+  opts = {
+   legacy_commands = false,
     workspaces = {
       {
         name = "personal",
         path = "~/vaults/personal",
       },
     },
-          checkbox = {
-        order = { " ", "x", ">", "~", "!" },  -- Define the cycling order
-        chars = {
-          [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
-          ["x"] = { char = "", hl_group = "ObsidianDone" },
-          [">"] = { char = "", hl_group = "ObsidianRightArrow" },
-          ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
-          ["!"] = { char = "", hl_group = "ObsidianImportant" },
-        },
+    checkbox = {
+      order = { " ", "x", ">", "~", "!" },  -- Define the cycling order
+      chars = {
+        [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+        ["x"] = { char = "", hl_group = "ObsidianDone" },
+        [">"] = { char = "", hl_group = "ObsidianRightArrow" },
+        ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+        ["!"] = { char = "", hl_group = "ObsidianImportant" },
       },
-          -- Enable UI enhancements
-      ui = {
-        enable = true,  -- Set to false to disable all UI features
-        update_debounce = 200,  -- Update delay in milliseconds
-        max_file_length = 5000,  -- Disable UI for files longer than this
-        
-        -- Bullets for lists
-        bullets = { char = "•", hl_group = "ObsidianBullet" },
-        
+    },
+    ui = {
+      enable = true,  -- Set to false to disable all UI features
+      update_debounce = 200,  -- Update delay in milliseconds
+      max_file_length = 5000,  -- Disable UI for files longer than this
+            -- Bullets for lists
+      bullets = { char = "•", hl_group = "ObsidianBullet" },
         -- External link icon
-        external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
-        
+      external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
         -- Reference text (for footnotes, etc.)
-        reference_text = { hl_group = "ObsidianRefText" },
-        highlight_text = { hl_group = "ObsidianHighlightText" },
-        tags = { hl_group = "ObsidianTag" },
-        block_ids = { hl_group = "ObsidianBlockID" },
-        
-        hl_groups = {
-          ObsidianTodo = { bold = true, fg = "#f78c6c" },
-          ObsidianDone = { bold = true, fg = "#89ddff" },
-          ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
-          ObsidianTilde = { bold = true, fg = "#ff5370" },
-          ObsidianImportant = { bold = true, fg = "#d73128" },
-          ObsidianBullet = { bold = true, fg = "#89ddff" },
-          ObsidianRefText = { underline = true, fg = "#c792ea" },
-          ObsidianExtLinkIcon = { fg = "#c792ea" },
-          ObsidianTag = { italic = true, fg = "#89ddff" },
-          ObsidianBlockID = { italic = true, fg = "#89ddff" },
-          ObsidianHighlightText = { bg = "#75662e" },
-        },
+      reference_text = { hl_group = "ObsidianRefText" },
+      highlight_text = { hl_group = "ObsidianHighlightText" },
+      tags = { hl_group = "ObsidianTag" },
+      block_ids = { hl_group = "ObsidianBlockID" },
+      hl_groups = {
+        ObsidianTodo = { bold = true, fg = "#f78c6c" },
+        ObsidianDone = { bold = true, fg = "#89ddff" },
+        ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
+        ObsidianTilde = { bold = true, fg = "#ff5370" },
+        ObsidianImportant = { bold = true, fg = "#d73128" },
+        ObsidianBullet = { bold = true, fg = "#89ddff" },
+        ObsidianRefText = { underline = true, fg = "#c792ea" },
+        ObsidianExtLinkIcon = { fg = "#c792ea" },
+        ObsidianTag = { italic = true, fg = "#89ddff" },
+        ObsidianBlockID = { italic = true, fg = "#89ddff" },
+        ObsidianHighlightText = { bg = "#75662e" },
       },
-      picker = {
-    -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
-    name = "telescope.nvim",
-    -- Optional, configure key mappings for the picker. These are the defaults.
-    -- Not all pickers support all mappings.
-    note_mappings = {
+    },
+    picker = {
+      name = "telescope.nvim",
+      note_mappings = {
       -- Create a new note from your query.
-      new = "<C-x>",
+        new = "<C-x>",
       -- Insert a link to the selected note.
-      insert_link = "<C-l>",
-    },
-    tag_mappings = {
+        insert_link = "<C-l>",
+      },
+      tag_mappings = {
       -- Add tag(s) to current note.
-      tag_note = "<C-x>",
+        tag_note = "<C-x>",
       -- Insert a tag at the current location.
-      insert_tag = "<C-l>",
+        insert_tag = "<C-l>",
+      },
     },
-  },
-
     daily_notes = {
       folder = "daily", -- Where daily notes are stored
       date_format = "%Y-%m-%d",
       alias_format = "%B %-d, %Y",
       template = "daily.md" -- Path to your template (relative to vault root)
-      },
-
+    },
     templates = {
       folder = "templates",
       date_format = "%Y-%m-%d",
       time_format = "%H:%M",
       -- A map for custom variables, the key should be the variable and the value a function
       substitutions = {},
-  },
-
-
+    },
     disable_frontmatter = true
-    -- see below for full list of options 👇
   },
 }
